2818fd33fba20c4d485f7d0cea7069eba8125c16
Jon Connor <jonconnor@live.ie>
2021-04-07 22:10:01 +0100

    Final edit 070421

--- Resources/cucumberReport/report.js
+++ Resources/cucumberReport/report.js
@@ -53,7 +53,7 @@ formatter.examples({
     },
     {
       "cells": [
-        "64",
+        "50",
         "Pass"
       ],
       "line": 18,
@@ -61,15 +61,15 @@ formatter.examples({
     },
     {
       "cells": [
-        "65",
-        "Merit"
+        "64",
+        "Pass"
       ],
       "line": 19,
       "id": "scores-and-grades;grades-are-calculated-from-the-score;;4"
     },
     {
       "cells": [
-        "79",
+        "65",
         "Merit"
       ],
       "line": 20,
@@ -77,11 +77,19 @@ formatter.examples({
     },
     {
       "cells": [
-        "80",
-        "Distinction"
+        "79",
+        "Merit"
       ],
       "line": 21,
       "id": "scores-and-grades;grades-are-calculated-from-the-score;;6"
+    },
+    {
+      "cells": [
+        "80",
+        "Distinction"
+      ],
+      "line": 22,
+      "id": "scores-and-grades;grades-are-calculated-from-the-score;;7"
     }
   ],
   "keyword": "Examples"
@@ -125,21 +133,21 @@ formatter.match({
   "location": "GradeSteps.is_entered_into_the_score_section(int)"
 });
 formatter.result({
-  "duration": 337131000,
+  "duration": 248515800,
   "status": "passed"
 });
 formatter.match({
   "location": "GradeSteps.the_grade_button_is_pressed()"
 });
 formatter.result({
-  "duration": 105200,
+  "duration": 73700,
   "status": "passed"
 });
 formatter.match({
   "location": "GradeSteps.fail_should_be_displayed()"
 });
 formatter.result({
-  "duration": 91900,
+  "duration": 62100,
   "status": "passed"
 });
 formatter.scenario({
@@ -147,6 +155,62 @@ formatter.scenario({
   "name": "Grades are calculated from the score",
   "description": "",
   "id": "scores-and-grades;grades-are-calculated-from-the-score;;3",
+  "type": "scenario",
+  "keyword": "Scenario Outline"
+});
+formatter.step({
+  "line": 11,
+  "name": "50 is entered into the score section",
+  "matchedColumns": [
+    0
+  ],
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 12,
+  "name": "the grade button is pressed",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 13,
+  "name": "Pass should be displayed",
+  "matchedColumns": [
+    1
+  ],
+  "keyword": "Then "
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "50",
+      "offset": 0
+    }
+  ],
+  "location": "GradeSteps.is_entered_into_the_score_section(int)"
+});
+formatter.result({
+  "duration": 283700,
+  "status": "passed"
+});
+formatter.match({
+  "location": "GradeSteps.the_grade_button_is_pressed()"
+});
+formatter.result({
+  "duration": 57500,
+  "status": "passed"
+});
+formatter.match({
+  "location": "GradeSteps.pass_should_be_displayed()"
+});
+formatter.result({
+  "duration": 60900,
+  "status": "passed"
+});
+formatter.scenario({
+  "line": 19,
+  "name": "Grades are calculated from the score",
+  "description": "",
+  "id": "scores-and-grades;grades-are-calculated-from-the-score;;4",
   "type": "scenario",
   "keyword": "Scenario Outline"
 });
@@ -181,28 +245,28 @@ formatter.match({
   "location": "GradeSteps.is_entered_into_the_score_section(int)"
 });
 formatter.result({
-  "duration": 258000,
+  "duration": 169300,
   "status": "passed"
 });
 formatter.match({
   "location": "GradeSteps.the_grade_button_is_pressed()"
 });
 formatter.result({
-  "duration": 74300,
+  "duration": 59200,
   "status": "passed"
 });
 formatter.match({
   "location": "GradeSteps.pass_should_be_displayed()"
 });
 formatter.result({
-  "duration": 89200,
+  "duration": 60200,
   "status": "passed"
 });
 formatter.scenario({
-  "line": 19,
+  "line": 20,
   "name": "Grades are calculated from the score",
   "description": "",
-  "id": "scores-and-grades;grades-are-calculated-from-the-score;;4",
+  "id": "scores-and-grades;grades-are-calculated-from-the-score;;5",
   "type": "scenario",
   "keyword": "Scenario Outline"
 });
@@ -237,28 +301,28 @@ formatter.match({
   "location": "GradeSteps.is_entered_into_the_score_section(int)"
 });
 formatter.result({
-  "duration": 236800,
+  "duration": 183600,
   "status": "passed"
 });
 formatter.match({
   "location": "GradeSteps.the_grade_button_is_pressed()"
 });
 formatter.result({
-  "duration": 79700,
+  "duration": 58800,
   "status": "passed"
 });
 formatter.match({
   "location": "GradeSteps.merit_should_be_displayed()"
 });
 formatter.result({
-  "duration": 97000,
+  "duration": 79600,
   "status": "passed"
 });
 formatter.scenario({
-  "line": 20,
+  "line": 21,
   "name": "Grades are calculated from the score",
   "description": "",
-  "id": "scores-and-grades;grades-are-calculated-from-the-score;;5",
+  "id": "scores-and-grades;grades-are-calculated-from-the-score;;6",
   "type": "scenario",
   "keyword": "Scenario Outline"
 });
@@ -293,28 +357,28 @@ formatter.match({
   "location": "GradeSteps.is_entered_into_the_score_section(int)"
 });
 formatter.result({
-  "duration": 217500,
+  "duration": 228500,
   "status": "passed"
 });
 formatter.match({
   "location": "GradeSteps.the_grade_button_is_pressed()"
 });
 formatter.result({
-  "duration": 89400,
+  "duration": 71400,
   "status": "passed"
 });
 formatter.match({
   "location": "GradeSteps.merit_should_be_displayed()"
 });
 formatter.result({
-  "duration": 108500,
+  "duration": 62000,
   "status": "passed"
 });
 formatter.scenario({
-  "line": 21,
+  "line": 22,
   "name": "Grades are calculated from the score",
   "description": "",
-  "id": "scores-and-grades;grades-are-calculated-from-the-score;;6",
+  "id": "scores-and-grades;grades-are-calculated-from-the-score;;7",
   "type": "scenario",
   "keyword": "Scenario Outline"
 });
@@ -349,21 +413,21 @@ formatter.match({
   "location": "GradeSteps.is_entered_into_the_score_section(int)"
 });
 formatter.result({
-  "duration": 209200,
+  "duration": 177000,
   "status": "passed"
 });
 formatter.match({
   "location": "GradeSteps.the_grade_button_is_pressed()"
 });
 formatter.result({
-  "duration": 113100,
+  "duration": 55900,
   "status": "passed"
 });
 formatter.match({
   "location": "GradeSteps.distinction_should_be_displayed()"
 });
 formatter.result({
-  "duration": 106000,
+  "duration": 90700,
   "status": "passed"
 });
 });
